8e02307edbf4c71bbea60d138af95008c0411923
Aj Narag <ajnarag25@gmail.com>
2022-08-05 14:42:41 +0800

    successfully implemented db in student

--- appointment/main_app/views.py
+++ appointment/main_app/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import redirect, render
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
+from .forms import *
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 def index(request):
@@ -14,7 +21,20 @@ def login_student(request):
     return render(request, "login_student.html")
 
 def signup_student(request):
-    return render(request, "signup_student.html")
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        messages.info(request,'Something went wrong!')
+        if form.is_valid():
+            messages.info(request,'Successfully Registered your Account!')
+            form.save()
+            return redirect('login_student')
+
+    context = {
+        'form': form
+    }
+   
+    return render(request, "signup_student.html", context)
 
 def signup_admin(request):
     return render(request, "signup_admin.html")
